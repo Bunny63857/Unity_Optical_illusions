3573bf713c715fbe305914c86d36d26e1ad541bb
Shouki Imai <ukiru77@gmail.com>
2021-05-22 18:52:42 +0900

    End : ライラックチェイサー
    機構の完成

--- Assets/Liac_chaser.cs
+++ Assets/Liac_chaser.cs
@@ -6,6 +6,8 @@ public class Liac_chaser : MonoBehaviour
 {
     public GameObject target;
     public GameObject[] spheres;
+    private int liac_num;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,8 @@ public class Liac_chaser : MonoBehaviour
             spheres[i] = (GameObject)Instantiate(Resources.Load("Prefab/Sphere_lilac"));
         }
         InitPos();
+        liac_num =0;
+        InvokeRepeating("Liac", 1, speed);
     }
 
 
@@ -32,5 +36,35 @@ public class Liac_chaser : MonoBehaviour
         {
             spheres[i].transform.position = target_pos + new Vector3(4*Mathf.Sin(2*Mathf.PI*i/spheres.Length), 4 * Mathf.Cos(2 * Mathf.PI * i / spheres.Length), 0);
         }
+    }
+
+    void Liac()
+    {
+        Spheres_On_Off(spheres[liac_num]);
+        liac_num++;
+        
+        if (liac_num == spheres.Length)
+        {
+            liac_num = 0;
+        }
+    }
+
+    void Spheres_On_Off(GameObject obj)
+    {
+        if (obj.activeSelf)
+        {
+            obj.SetActive(false);
+            StartCoroutine(On_Sphere(obj));
+        }
+        else
+        {
+            obj.SetActive(true);
+        }
+    }
+
+    IEnumerator On_Sphere(GameObject obj)
+    {
+        yield return new WaitForSeconds(speed);
+        obj.SetActive(true);
     }
 }
